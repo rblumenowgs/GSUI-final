70d58c4cab7d6b63ac69bf81886091a1f4034851
rblumenowgs <ryan.blumenow@globalstratos.com>
2021-12-20 23:10:57 +0200

    Update multipage2gs.py

--- multipage2gs.py
+++ multipage2gs.py
@@ -226,6 +226,35 @@ class MultiPage:
 
         footercontent = '''Copyright (c) Globalstratos 2021'''
 
+        footer="""<style>
+        a:link , a:visited{
+        color: blue;
+        background-color: transparent;
+        text-decoration: underline;
+        }
+
+        a:hover,  a:active {
+        color: red;
+        background-color: transparent;
+        text-decoration: underline;
+        }
+
+        .footer {
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        background-color: white;
+        color: black;
+        text-align: center;
+        }
+        </style>
+        <div class="footer">
+        <p>Copyright (c) Globalstratos 2021<a style='display: block; text-align: center;' href="https://www.globalstratos.com/" target="_blank">Globalstratos</a></p>
+        </div>
+        """
+        st.markdown(footer,unsafe_allow_html=True)
+
         ### HIDING HAMBURGER MENU
         
         # hide_streamlit_style = """
